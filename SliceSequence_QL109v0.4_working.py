50bdf5cdfc67c4592c0a9b493dcc8c3b8f6593ea
Chao <snail123815@users.noreply.github.com>
2017-06-14 21:39:50 +0200

    Add files via upload

---
+++ SliceSequence_QL109v0.4_working.py
@@ -0,0 +1,154 @@
+from Bio import SeqIO
+import pathlib # for checking the existance of a file
+
+
+def user_input():
+	def input_num(promote):
+		try:
+			num = int(input(promote))
+			return num
+		except ValueError:
+			print('Please enter a number!\n')
+			return input_num(promote)
+	quest = input('Do you want 1 gene(a) or several genes in a row?(b):')
+	while quest != "a" and quest != "b":
+		quest = input('a or b please:')
+	
+	# Take input from user
+	if quest == "a":
+		sc = input_num('Scaffold number: ')
+		target_start = input_num('ORF number: ')
+		target_end = target_start
+	else:
+		sc = input_num('Scaffold number: ')
+		target_start = input_num('ORF number of the start gene: ')
+		target_end = input_num('ORF number of the end gene: ')
+	flanking = input_num('Please enter a number for flanking:')
+	return(sc, target_start, target_end, flanking)
+
+# Clear the log file if exist
+def clear_log(logfile):
+	if pathlib.Path(logfile).is_file():
+		log_handle = open(logfile, 'w')
+		log_handle.close()
+		write_log('Previous log cleaned\n')
+
+	
+def write_log(logstr, end = None):
+	log_handle = open(logfile, 'a')
+	print(logstr, end = end, file = log_handle)
+	print(logstr, end = end)
+	log_handle.close()
+
+def slice(sc, target_start, target_end, flanking):
+	# Convert numbers to strings needed in output
+	sc_str = str(sc).zfill(2)
+	target_start_str = str(target_start).zfill(4)
+	target_end_str = str(target_end).zfill(4)
+	flanking_str = f'f{flanking}' if flanking != 0 else ''
+
+	# Creat a global vairable that can be accessed by write_log function
+	global logfile
+	logfile = f"D:/WORKs/Resource_QL109/GenomeSlices/{sc_str}_{target_start_str}-{target_end_str}{flanking_str}.log"
+	clear_log(logfile)
+
+	# Read the file
+	sample = SeqIO.read(f"D:/WORKs/Resource_QL109/Sequencing/sc{sc_str}.embl","embl")
+	write_log('The imported sequence has {0} features.\n'.format(len(sample.features)))
+	
+	# Check if the data is actually imported
+	genes_start = sample.features[target_start].location.start
+	genes_end = sample.features[target_end].location.end
+	write_log(f'The gene(s) wanted from {genes_start} to {genes_end}.\n')
+	
+	# Set the desired start and end point on the imported sequence, and creat a sub sample
+	start = genes_start - flanking
+	end = genes_end + flanking
+	sub_sample = sample[start:end]
+	write_log(f'Target seqID:\tSC{sc_str}_ORF{target_start_str} to {target_end_str}\nFlanking:{flanking}\n')
+	
+	# Some extra genes might appear in the flanking regions that will be included in the final file, inform user
+	genes_needed = target_end - target_start + 1
+	genes_loaded = len(sub_sample.features)
+	write_log(f'There are {genes_needed} genes needed, {genes_loaded - genes_needed} extra genes in the flanking reigon.\n')
+	
+	# There is no orf information for unknown protein in the file, so we need to determin the first protein's orf number in case of unknown protein in the sliced sequence.
+	first_orfnum = target_start - len(sample[start:genes_start].features)
+	
+	write_log('################# Genes in the output file #################\n')
+	for feat_num, feat in enumerate(sub_sample.features):
+		write_log(f'No.{feat_num+1}:')
+		if feat.qualifiers['gene'][0] != 'unknown': # if this feature have been assigned to a protein name
+			try:
+				feat.qualifiers['inference'] = [] # Set up 'inference' field to convert convertable information to genbank official format, 'inference' qualifier is more accurate in describing the feature. Here for QL109 we have blastp_match and domain match, so it is a list.
+				for note in feat.qualifiers['note']:
+					if note.startswith('ORF '): # Every feature has a ORF number in QL109 embl files, write the information on screen, together with protein name and similarity value
+						write_log(note, end = ': ')
+						write_log(feat.qualifiers['gene'][0] + '\n\t' + feat.qualifiers['similarity'][0])
+						ORF_num = note.split(' ')[2]
+						feat.qualifiers['note'].remove(note) # I will put the information in valid qualifier 'protein_id' and 'locus_tag'
+					elif note.startswith('homologous to '):
+						refseq_id = note.split(' ')[2].split('|')[1].split('_')[1]
+						feat.qualifiers['inference'].append(f'similar to AA sequence:UniProtKB:{refseq_id}')
+				feat.qualifiers['note'].append(f'blastp_match: {"; ".join(feat.qualifiers["blastp_match"])}, similarity: {"; ".join(feat.qualifiers["similarity"])}') # Put redundant information in another note
+				del feat.qualifiers["blastp_match"], feat.qualifiers["similarity"]
+				
+				if 'domain' in feat.qualifiers:
+					pfams = []
+					for domain in feat.qualifiers['domain']:
+						pfams.append(domain.split(' ')[1])
+						feat.qualifiers['note'].append(f'domain: {domain}') # Put redundant information in another note
+					feat.qualifiers['inference'].append(f'protein motif:PFAM:{",".join(pfams)}')
+					del feat.qualifiers['domain']
+
+
+			except:
+				write_log('Something wrong?')
+				raise
+		else: # if the gene has no homologe, calculate orf number here.
+			ORF_num = first_orfnum + feat_num
+			write_log(f'ORF number {ORF_num} on the contig: Unknown protein')
+			
+			
+		feat.qualifiers['codon_start'] = 1
+		feat.qualifiers['transl_table'] = 11
+		locus_tag = f'QL109_{sc}{ORF_num}'
+		protein_id = locus_tag[6:]
+		feat.qualifiers['protein_id'] = protein_id
+		feat.qualifiers['product'] = feat.qualifiers['gene'][0]
+		del feat.qualifiers['gene']
+		feat.qualifiers['locus_tag'] = locus_tag
+		strand = f'{feat.location.strand:+}'[0]
+		write_log(f'\tStrand: {strand}\n')
+		
+	# Rename the sample, if the name is too long, shorten it
+	LOCUS = f"{sc_str}_{target_start}+{genes_loaded}{flanking_str}"
+	if len(LOCUS)<=16:
+		sub_sample.name = LOCUS
+	else:
+		LOCUS = f"{sc_str}_{target_start}+{genes_loaded}{flanking_str[0]}"
+		sub_sample.name = LOCUS
+	
+	output_file = f"D:/WORKs/Resource_QL109/GenomeSlices/{sc_str}_{target_start_str}-{target_end_str}{flanking_str}.gb"
+	
+	if pathlib.Path(output_file).is_file():
+		caution = 'Caution file overwriten:\n'
+	else:
+		caution = ''
+	
+	SeqIO.write(sub_sample, output_file, "genbank")
+	# Should return 1 if successful
+	# Show ending information
+	write_log(f"\n########## Output files ##########\n\n{caution}{output_file}\n{logfile}\n")
+
+# Test output:
+# slice(3,1244,1251, 100000)
+slice(*user_input())
+
+# pause the program
+
+input('Press <ENTER> to continue.')
+
+exit()
+
+
---
+++ blast_QL109.py
@@ -0,0 +1,108 @@
+import os
+from Bio.Seq import Seq
+
+blastn_program_path = 'D:\\Program Files\\NCBI\\blast-2.6.0+\\bin\\blastn.exe'
+blastp_program_path = 'D:\\Program Files\\NCBI\\blast-2.6.0+\\bin\\blastp.exe'
+output_path = os.path.join(os.getcwd(),'temp\\')
+nucldb_path = 'D:\\WORKs\\Resource_QL109\\blastdb_QL109\\nt_QL109'
+protdb_path = 'D:\\WORKs\\Resource_QL109\\blastdb_QL109\\prot_QL109'
+
+# determine seq type from user input
+def seq_type():
+	seq_type_in = input('Protein(p) or nucleic(n)?')
+	print(seq_type_in)
+	if seq_type_in != 'p' and seq_type_in != 'n':
+		seq_type = ''
+		print("Don't understand, I will determine by myself.\n")
+	elif seq_type_in == 'p':
+		seq_type = 'prot'
+	elif seq_type_in == 'n':
+		seq_type = 'nucl'
+	return(seq_type)
+
+def input_seqs(seq_type):
+	num_seqs = 0
+	seq_titles = []
+	temp_input_seq_file = os.path.join(output_path, 'temp_input_seq.fa')
+	
+	print('Please enter/paste your gene(s)/protein(s) in fasta format.\nThen start a new line, press Ctrl-Z(or D) + <Enter> to save it.\nInput:')
+	input_seqs = []
+	while True:
+		try:
+			line = input("")
+		except EOFError:
+			break
+		input_seqs.append(line)
+	with open(temp_input_seq_file, 'w') as input_file_handle:
+		for line in input_seqs:
+			if line.startswith('>'):
+				input_file_handle.write('\n'+line+'\n')
+				num_seqs += 1
+				seq_titles.append(line[1:])
+			else:
+				input_file_handle.write(line)
+				while num_seqs == 1 and seq_type == '':
+					from Bio.Data.CodonTable import TranslationError
+					try:
+						for i in range(len(line)-2):
+							test_seq = line[i:]
+							test_seq += 'n' * (3 - len(test_seq) % 3)
+							Seq(test_seq).translate()
+						seq_type = 'nucl'
+					except TranslationError:
+						seq_type = 'prot'
+	return temp_input_seq_file, num_seqs, seq_titles, seq_type
+
+
+def blastn(temp_input_seq_file, output_path, outfile):			
+	from Bio.Blast.Applications import NcbiblastnCommandline
+	out = os.path.join(output_path, outfile)
+	run_blastn = NcbiblastnCommandline(blastn_program_path,
+										query = temp_input_seq_file, 
+										out = out,
+										# outfmt = 5, 
+										db = nucldb_path, 
+										remote = False,
+										# evalue = 1e-30
+										)
+	print(f'Running BLASTN command:\n{run_blastn}')
+	stdout, stderr = run_blastn()
+	if stderr == '':
+		print(f'\nSuccess!!\n{out}\n')
+	from subprocess import run
+	try:
+		run(['notepad', out])
+	except:
+		pass
+
+def blastp(temp_input_seq_file, output_path, outfile):			
+	from Bio.Blast.Applications import NcbiblastpCommandline
+	out = os.path.join(output_path, outfile)
+	run_blastp = NcbiblastpCommandline(blastp_program_path,
+										query = temp_input_seq_file, 
+										out = out,
+										# outfmt = 5, 
+										db = protdb_path, 
+										remote = False,
+										# evalue = 1e-30
+										)
+	print(f'Running BLASTP command:\n{run_blastp}')
+	stdout, stderr = run_blastp()
+	if stderr == '':
+		print(f'\nSuccess!!\n{out}\n')
+	from subprocess import run
+	try:
+		run(['notepad', out])
+	except:
+		pass
+
+		
+temp_input_seq_file, num_seqs, seq_titles, seq_type = input_seqs(seq_type())
+import re
+file_name = re.sub('[^\w_.)( -]', '.', seq_titles[0])[:30]
+if seq_type == 'prot':
+	blastp(temp_input_seq_file, output_path, outfile = f'{file_name}_{num_seqs if num_seqs != 1 else ""}{"seqs_"if num_seqs != 1 else ""}blastp_QL109.out')
+elif seq_type == 'nucl':
+	blastn(temp_input_seq_file, output_path, outfile = f'{file_name}_{num_seqs if num_seqs != 1 else ""}{"seqs_"if num_seqs != 1 else ""}blastn_QL109.out')
+else:
+	print('No BLAST done')
