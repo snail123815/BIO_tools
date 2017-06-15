056e14ce5cd8dacb2695d59a2b983e5f0978ad55
Chao <snail123815@users.noreply.github.com>
2017-06-15 12:55:13 +0200

    Adapted to intergate protein id in proteomics expression file
    The excel file I alread made can take a scaffolad_888_ORF_8888 input to show the expression profile.

--- search_word_in_biofile_QL109.py
+++ search_word_in_biofile_QL109.py
@@ -7,14 +7,16 @@ import write_file
 genome_dir = r'D:\WORKs\Resources\Resource_QL109\Genome_QL109'
 search_word = ''
 found_num = 0
+found_proteins = []
 
 while search_word == '':
 	search_word = input('What do you want to search? (case insensitive, can be regular expression)\n')
 	print("="*60)
 	genome_name = genome_dir.split("\\")[-1]
-	out_file = f'D:/WORKs/BIOBIO/temp/{genome_name}_{write_file.clean_str(search_word)}.txt'
+	out_file = f'D:/WORKs/Resources/Resource_QL109/temp/genome_txt_search/{genome_name}_{write_file.clean_str(search_word)}.txt'
 for file in os.listdir(genome_dir):
 	if file.endswith('.embl') and file.startswith('sc') and len(file) == 9:
+		sc = file[2:4].zfill(3)
 		file_path = os.path.join (genome_dir, file)
 		my_seq = SeqIO.read(file_path, 'embl')
 		find = re.compile(search_word, re.IGNORECASE)
@@ -28,10 +30,23 @@ for file in os.listdir(genome_dir):
 						if find.search(txt) != None:
 							output_str = f'{feat}\nMatch:\t{txt}\nOn:\t{file}\n{"-"*60}'
 							found = True
-							found_num += 1
-							write_file.write(output_str, out_file)
+							txt_str = txt
+					if found and 'translation' in feat.qualifiers:
+						for note in feat.qualifiers['note']:
+							if note.startswith('ORF number'):
+								orf = note.split(' ')[2]
+						protein_id = f'scaffolad_{sc}_ORF_{orf}'
+						found_proteins.append(protein_id)
+						output_str = f'{feat}\nMatch:\t{txt_str}\nID:\t{protein_id}\n{"-"*60}'
+					if found:
+						write_file.write(output_str, out_file)
+						found_num += 1
 	else:
 		pass
 
-write_file.write(f'\nFound {found_num} features with the word "{search_word}".\n{"="*60}', out_file)
+write_file.write(f'\nFound {found_num} features with the word "{search_word}", protein ids:', out_file)
+for index, id in enumerate(found_proteins):
+	output_str = f'{index+1}:\t{id}'
+	write_file.write(output_str, out_file, end = '\n')
+write_file.write(f'\n{"="*60}', out_file)
 write_file.open_in_notepad(out_file)
