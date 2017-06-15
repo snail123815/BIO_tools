8f92500d45e93f84652be265c170d8b4e124d3b7
Chao <snail123815@users.noreply.github.com>
2017-06-15 11:13:10 +0200

    Refinament of search
    write_file module added for furture use to write log on screen and file at the same time

--- search_word_in_biofile_QL109.py
+++ search_word_in_biofile_QL109.py
@@ -2,16 +2,21 @@ from Bio import SeqIO
 import re
 import os
 
+import write_file
+
 genome_dir = r'D:\WORKs\Resources\Resource_QL109\Genome_QL109'
-search_word = r''
+search_word = None
 found_num = 0
+
 for file in os.listdir(genome_dir):
 	if file.endswith('.embl') and file.startswith('sc') and len(file) == 9:
 		file_path = os.path.join (genome_dir, file)
 		my_seq = SeqIO.read(file_path, 'embl')
-		if search_word == r'':
+		if search_word == None:
 			search_word = input('What do you want to search? (case insensitive, can be regular expression)\n')
 			print("="*60)
+			genome_name = genome_dir.split("\\")[-1]
+			out_file = f'D:/WORKs/BIOBIO/temp/{genome_name}_{write_file.clean_str(search_word)}.txt'
 		else:
 			find = re.compile(search_word, re.IGNORECASE)
 			for feat in my_seq.features:
@@ -22,9 +27,12 @@ for file in os.listdir(genome_dir):
 					else:
 						for txt in feat.qualifiers[tag]:
 							if find.search(txt) != None:
-								print(feat,f'\nMatch:\t{txt}\nOn:\t{file}\n{"-"*60}')
+								output_str = f'{feat}\nMatch:\t{txt}\nOn:\t{file}\n{"-"*60}'
 								found = True
 								found_num += 1
+								write_file.write(output_str, out_file)
 	else:
 		pass
-print(f'\nFound {found_num} features with the word "{search_word}".\n{"="*60}')
+
+write_file.write(f'\nFound {found_num} features with the word "{search_word}".\n{"="*60}', out_file)
+write_file.open_in_notepad(out_file)
---
+++ write_file.py
@@ -0,0 +1,21 @@
+
+def clear(file_path):
+	import pathlib
+	if pathlib.Path(file_path).is_file():
+		with open(file_path, 'w') as clear_handle:
+			write(f'Previous {file_path} cleaned\n', file_path)
+
+	
+def write(string, file_path, end = None):
+	with open(file_path, 'a') as write_handle:
+		print(string, end = end, file = write_handle)
+		print(string, end = end)
+
+def open_in_notepad(file_path):
+	import subprocess
+	run_notepad = subprocess.run(['notepad', file_path], shell = True)
+	# print(run_notepad)
+	
+def clean_str(str):
+	import re
+	return re.sub('[^\w_.)( -]', '.', str)
