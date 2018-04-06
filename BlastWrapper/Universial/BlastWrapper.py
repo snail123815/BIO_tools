b520e5ef0ba2ee8827d259ee68a2802ece8510b0
Chao <snail123815@users.noreply.github.com>
2018-04-06 16:01:19 +0200

    Finalized.
    Now able to have multiple database in /GenomeProteomeDatabase folder, set it up using ./Parameters.csv file.  
    BLAST parameters:  
    * outfmt
    * evalue
    * word_size
    you can also setup using  ./Parameters.csv file.

--- BlastWrapper/Universial/BlastWrapper.py
+++ BlastWrapper/Universial/BlastWrapper.py
@@ -1,31 +1,37 @@
-import os, platform
+from Bio.Blast.Applications import NcbiblastpCommandline, NcbiblastnCommandline, NcbiblastxCommandline, NcbitblastnCommandline, NcbitblastxCommandline
+from os import path, stat, environ, getenv, makedirs
+from time import time
+from subprocess import Popen
+from sys import exit
+from platform import system
+from csv import reader as csvreader
+from os import cpu_count
+import pathlib
+import re
 
-cwd = os.path.dirname(os.path.realpath(__file__))
-output_path = os.path.join(cwd, "BLAST_Results")
+cwd = path.dirname(path.realpath(__file__))
+output_path = path.join(cwd, "BLAST_Results")
+temp_input_seq_file = path.join(output_path, 'temp_input_seq.fa')
+input_seq_file = path.join(cwd, "BLAST_INPUT_fasta.txt")
+print(f"\nPut your input <fasta format> sequence (multiple sequences allowed) in <{input_seq_file}>\nOR paste your sequence later.")
 
-nucldb = "nt"
-protdb = "prot"
-outputFormat = 0
-evalue = 1
-num_threads = os.cpu_count()
-
-if platform.system() in ['Darwin', 'Linux']:
+if system() in ['Darwin', 'Linux']:
 	print('\nThis wrapper needs blast programs setted up in your PATH.')
 	blastn_program_path  = 'blastn'
 	blastp_program_path  = 'blastp'
 	blastx_program_path  = 'blastx'
 	tblastn_program_path = 'tblastn'
 	tblastx_program_path = 'tblastx'
-	if platform.system() == 'Linux':
+	if system() == 'Linux':
 		fileReader = ['gedit']
 	else:
 		fileReader = ['open', '-a', 'TextWrangler']
 else: # 'Windows'
-	blastn_program_path  = os.path.join(cwd, "blastProgram/blastn.exe")
-	blastp_program_path  = os.path.join(cwd, "blastProgram/blastp.exe")
-	blastx_program_path  = os.path.join(cwd, "blastProgram/blastx.exe")
-	tblastn_program_path = os.path.join(cwd, "blastProgram/tblastn.exe")
-	tblastx_program_path = os.path.join(cwd, "blastProgram/tblastx.exe")
+	blastn_program_path  = path.join(cwd, "blastProgram/blastn.exe")
+	blastp_program_path  = path.join(cwd, "blastProgram/blastp.exe")
+	blastx_program_path  = path.join(cwd, "blastProgram/blastx.exe")
+	tblastn_program_path = path.join(cwd, "blastProgram/tblastn.exe")
+	tblastx_program_path = path.join(cwd, "blastProgram/tblastx.exe")
 	fileReader = ['notepad']
 
 def seq_type():
@@ -41,29 +47,23 @@ Your decision: ''')
 
 		
 def input_seqs(seq_type):
-	num_seqs = 0
 	seq_titles = []
-	temp_input_seq_file = os.path.join(output_path, 'temp_input_seq.fa')
-	input_seq_file = os.path.join(cwd, "BLAST_INPUT_fasta.txt")
-	print(f"\nPut your input <fasta> sequence in <{input_seq_file}>\nOR paste your sequence later (multiple sequences allowed).\n")
 		# This block is to find whether to perform search with existing file
-	import pathlib
 	last_blast = "n"
 	input_seq_by_file = "n"
 
-	if pathlib.Path(input_seq_file).is_file() and os.stat(input_seq_file).st_size != 0:
+	if pathlib.Path(input_seq_file).is_file() and stat(input_seq_file).st_size != 0:
 		input_seq_by_file = None
 		while input_seq_by_file != 'y' and input_seq_by_file != 'n':
-			input_seq_by_file = input(f'Found input file <{input_seq_file}>\nUse as input?(y/n)')
-	if input_seq_by_file == 'n' and pathlib.Path(temp_input_seq_file).is_file() and os.stat(temp_input_seq_file).st_size != 0:
+			input_seq_by_file = input(f'\nFound input file, use as input?(y/n)')
+	if input_seq_by_file == 'n' and pathlib.Path(temp_input_seq_file).is_file() and stat(temp_input_seq_file).st_size != 0:
 		last_blast = None
 		while last_blast != 'y' and last_blast != 'n':
-			last_blast = input(f'Found last search in <{temp_input_seq_file}>\nUse as input?(y/n)')
+			last_blast = input(f'Found last search in, use as input?(y/n)')
 	
 	input_sequences = []
-
 	if input_seq_by_file == 'n' and last_blast == 'n':
-		print('\nPlease enter/paste your gene(s)/protein(s) in <fasta> format\nTo finish, start a new line, press Ctrl-Z (windows) OR Ctrl-D(OSX/Linux) then press <Enter> to save it.\nInput:')
+		print('\nPlease enter/paste your gene(s)/protein(s)\n\tFasta header is recommended.\n\tUse Fasta headers for multiple sequences.\nTo finish input: Start a new line, press Ctrl-Z (windows) OR Ctrl-D(OSX/Linux), press <Enter> to save it.\nInput:')
 		while True:
 			try:
 				line = input("")
@@ -76,9 +76,11 @@ def input_seqs(seq_type):
 	elif input_seq_by_file == 'y':
 		with open(input_seq_file, 'r') as f:
 			input_sequences = f.readlines()
-		print(f"\nCopy sequence(s) from <{input_seq_file}> to <{temp_input_seq_file}>\n")
-	
-	
+		print(f"\nCopying sequence(s) from <{input_seq_file}> to <{temp_input_seq_file}>\n")
+		
+	if not input_sequences[0].startswith('>'):
+		input_sequences.insert(0, f">Input_Sequence{str(time()).split('.')[0]}")
+	num_seqs = 0
 	with open(temp_input_seq_file, 'w') as input_file_handle:
 		for line in input_sequences:
 			if line.startswith('>'):
@@ -89,34 +91,55 @@ def input_seqs(seq_type):
 				input_file_handle.write(line.strip() + '\n')
 	return temp_input_seq_file, num_seqs, seq_titles, seq_type
 
-def doBlast(seq_type, temp_input_seq_file, output_path, file_name):	
-	from Bio.Blast.Applications import NcbiblastpCommandline, NcbiblastpCommandline, NcbiblastxCommandline, NcbitblastnCommandline, NcbitblastxCommandline
+def doBlast(seq_type, temp_input_seq_file, output_path, file_name, num_seqs):
 	
-	outputFile = f'{file_name}_{num_seqs if num_seqs != 1 else ""}{"seqs_"if num_seqs != 1 else ""}'
-	blastProgramsAndOutput = {'p' :[NcbiblastpCommandline,  blastp_program_path , protdb, f'blastp_protein_{outputFile}.txt'],
-							  'n' :[NcbiblastpCommandline,  blastn_program_path , nucldb, f'blastn_nucleotide_{outputFile}.txt'],
-							  'x' :[NcbiblastxCommandline,  blastx_program_path , protdb, f'blastx_transNucl2Prot_{outputFile}.txt'],
-							  'tn':[NcbitblastnCommandline, tblastn_program_path, nucldb, f'tblastn_transProt2Nucl_{outputFile}.txt'],
-							  'tx':[NcbitblastxCommandline, tblastx_program_path, nucldb, f'tblastx_transNucl2transNucl_{outputFile}.txt']
+	# Define parameters for blast command
+	num_threads = cpu_count()
+	
+	## Read from file part of parameters: 'nucldb', 'protdb', 'outfmt', 'evalue', 'word_size'
+	parameterDict = dict(nucldb='nucl', protdb='prot', outfmt=None, evalue=None, word_size=None)
+	with open(path.join(cwd, 'Parameters.csv'), 'r') as parametersCSV:
+		paras = csvreader(parametersCSV)
+		for i, row in enumerate(paras):
+			if row[0] in ['nucldb', 'protdb', 'outfmt', 'evalue', 'word_size'] and row[1].strip() != '':
+				parameterDict[row[0]] = row[1]
+	nucldb    = parameterDict['nucldb'   ]
+	protdb    = parameterDict['protdb'   ]
+	outfmt    = parameterDict['outfmt'   ]
+	evalue    = parameterDict['evalue'   ]
+	word_size = parameterDict['word_size']
+	## Define parameters: blast_program, 'db', 'out'
+	dynamicNamePart = f'{file_name}{("_"+str(num_seqs)+"seqs") if num_seqs != 1 else ""}'
+	blastProgramsAndOutput = {'p' :[NcbiblastpCommandline,  blastp_program_path , protdb, f'blast_protDB_{protdb}_{dynamicNamePart}.txt'],
+							  'n' :[NcbiblastnCommandline,  blastn_program_path , nucldb, f'blast_nuclDB_{nucldb}_{dynamicNamePart}.txt'],
+							  'x' :[NcbiblastxCommandline,  blastx_program_path , protdb, f'blast_ProtDB_{protdb}_transNucl_{dynamicNamePart}.txt'],
+							  'tn':[NcbitblastnCommandline, tblastn_program_path, nucldb, f'blast_NuclDB_{nucldb}_transProt_{dynamicNamePart}.txt'],
+							  'tx':[NcbitblastxCommandline, tblastx_program_path, nucldb, f'blast_NuclDB_{nucldb}_transNucl_{dynamicNamePart}.txt']
 							 }
 	blast_program = blastProgramsAndOutput[seq_type][1]
 	db            = blastProgramsAndOutput[seq_type][2]
-	out = os.path.join(output_path, blastProgramsAndOutput[seq_type][3])
+	out = path.join(output_path, blastProgramsAndOutput[seq_type][3])
+	
+	# Put arguments together
 	arguments = {'cmd'        : blast_program,
 				 'query'      : temp_input_seq_file,
 				 'out'        : out,
-				 'outfmt'     : outputFormat, 
 				 'db'         : db, 
-				 'remote'     : False,
-				 'evalue'     : evalue,
 				 'num_threads': num_threads
 				}
+	if outfmt != None:
+		arguments['outfmt'] = outfmt
+	if evalue != None:
+		arguments['evalue'] = evalue
+	if word_size != None:
+		arguments['word_size'] = word_size
+
+	# Run blast command line
 	run_blast = blastProgramsAndOutput[seq_type][0](**arguments)
 	print(f'\nRunning BLAST command:\n{run_blast}')
 	stdout, stderr = run_blast()
 	if stderr == '':
 		print(f'\nSuccess!! Find your result here:\n{out}\n')
-	from subprocess import Popen
 	try:
 		Popen(([*fileReader, out]))
 	except:
@@ -124,40 +147,38 @@ def doBlast(seq_type, temp_input_seq_file, output_path, file_name):
 
 # Database path configuration
 print('\nSetting up BLASTDB environment variable...')
-if "BLASTDB" in os.environ:
-	origionalBLASTDBenv = os.getenv('BLASTDB')
+if "BLASTDB" in environ:
+	origionalBLASTDBenv = getenv('BLASTDB')
 else:
 	origionalBLASTDBenv = False
 
 try:
-	os.environ['BLASTDB'] = os.path.join(cwd, "GenomeProteomeDatabase")
+	environ['BLASTDB'] = path.join(cwd, "GenomeProteomeDatabase")
 	print('Success, environment variable BLASTDB will be restored in the end.\n')
 except:
 	print('BLASTDB environment variable set up failed.')
-	input('')
-	from sys import exit
+	input('<Enter> to exit')
 	exit()
 
 # Do blast, return path configuration at any time
 try:
-	if not os.path.exists(output_path):
-		os.makedirs(output_path)
+	if not path.exists(output_path):
+		makedirs(output_path)
 	temp_input_seq_file, num_seqs, seq_titles, seq_type = input_seqs(seq_type())
-	import re
 	file_name = re.sub('[^\w_.)( -]', '.', seq_titles[0])[:30]
 	try:
-		doBlast(seq_type, temp_input_seq_file, output_path, file_name)
+		doBlast(seq_type, temp_input_seq_file, output_path, file_name, num_seqs)
 	except Exception as e:
 		print('No blast done!')
 		print(e)
 except Exception as e:
 	print(e)
-	input("")
+	input("<Enter> to exit")
 finally:
-	print('Restoring environment varialbe... ', end = '')
+	print('\nRestoring environment varialbe... ', end = '')
 	if origionalBLASTDBenv:
-		os.environ['BLASTDB'] = origionalBLASTDBenv
+		environ['BLASTDB'] = origionalBLASTDBenv
 	else:
-		os.environ.pop('BLASTDB')
+		environ.pop('BLASTDB')
 	print('Restored!')
-	input('')
+	input('<Enter> to exit')
--- BlastWrapper/Universial/GenomeProteomeDatabase/GenomeProteomeDatabase.md
+++ BlastWrapper/Universial/GenomeProteomeDatabase/GenomeProteomeDatabase.md
@@ -1,7 +1,2 @@
-This folder should contain any blast database. The file name of database should be:  
-* nt.nhr
-* nt.nin
-* nt.nsq
-* prot.nhr
-* prot.nin
-* prot.nsq
+This folder should contain any blast database.
+To choose one, use Parameters.csv file in the parent folder.
