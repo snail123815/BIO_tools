0b742e294f5dd3b666778a8c22fd5d1df0447c05
Chao DU <durand.dc@gmail.com>
2019-06-30 16:07:04 +0200

    My tools as bioinformatician

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # BIO_tools
+
+NOT really a project.
+
 As a learning process, these are the tools that I built specifically for my study of MBT76, featuring Biopython module 1.68 upwards.
 I hope these codes can be easily adapted to other genomes.
 
--- genebankToGff.py
+++
@@ -1,7 +0,0 @@
-## convert genbank to GFF formart
-
-from Bio import SeqIO
-from BCBio import GFF
-
-with open('/Users/durand.dc/Documents/works/Resources/Resource_M145/M145.gb','r') as gbFile, open('/Users/durand.dc/Documents/works/Resources/Resource_M145/M145.gff','w') as gffFile:
-		GFF.write(SeqIO.parse(gbFile,'genbank'), gffFile)
