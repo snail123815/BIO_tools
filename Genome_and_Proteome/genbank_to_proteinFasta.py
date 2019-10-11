e2396e8b5b1e3b1c9fcf90dc8d4a77d8cd327602
Chao DU <durand.dc@gmail.com>
2019-10-11 15:43:44 +0200

    Universify

--- Genome_and_Proteome/genbank_to_proteinFasta.py
+++ Genome_and_Proteome/genbank_to_proteinFasta.py
@@ -1,41 +1,39 @@
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
 
-genbankSeq = SeqIO.parse(
-    '/Users/durand.dc/Documents/works/Resources/Strains and Plasmids/MBT84.gbk', 'genbank')
 
-proteins = {}
-
-for seq in genbankSeq:
+def getProteins(seq):
+    """Extract proteins from a SeqRecord. If your input file have multiple contigs, do a loop"""
+    proteins = []
     for feat in seq.features:
         if feat.type == 'CDS':
-            gene = feat.qualifiers['gene'][0].capitalize(
-            ) if 'gene' in feat.qualifiers else None
+            gene = feat.qualifiers['gene'][0] if 'gene' in feat.qualifiers else None
+            # make upper case for this is a protein
+            if gene != None:
+                gene = gene[0].upper() + gene[1:]
             locusTag = feat.qualifiers['locus_tag'][0] if 'locus_tag' in feat.qualifiers else None
-            proteinName = '_'.join(filter(None, (locusTag, gene))).replace(' ', '_')
-            proteins[proteinName] = feat.qualifiers['translation'][0]
+            proteinName = ', '.join(filter(None, (gene, locusTag))).replace(' ', '_')
+            prot = SeqRecord(Seq(feat.qualifiers['translation'][0]),
+                             id=locusTag, description=proteinName)
+            proteins.append(prot)
+    return proteins
 
-# for feat in genbankSeq.features:
-#     hasTranslationQualifier = 'translation' in feat.qualifiers
-#     hasTranslationNote = False
-#     if not hasTranslationQualifier:
-#         if 'note' in feat.qualifiers:
-#             for note in feat.qualifiers['note']:
-#                 if note.startswith('/translation'):
-#                     hasTranslationNote = True
-#                     translation = note.split('=')[-1]
-#     else:
-#         translation = feat.qualifiers['translation'][0]
-#
-#     if hasTranslationQualifier or hasTranslationNote:
-#         prefix = 'pET28a'
-#         proteinId = feat.qualifiers['protein_id'][0] if 'protein_id' in feat.qualifiers else None
-#         gene = feat.qualifiers['gene'][0].capitalize() if 'gene' in feat.qualifiers else None
-#         label = feat.qualifiers['label'][0] if 'label' in feat.qualifiers and not gene else None
-#         proteinName = '_'.join(filter(None, (prefix,proteinId,gene,label))).replace(' ', '_')
-#         proteins[proteinName] = translation
 
-with open('/Users/durand.dc/Documents/works/Resources/Strains and Plasmids/MBT84.proteome.fasta', 'w') as output:
-    for protein in proteins:
-        output.write(f'>{protein}\n')
-        output.write(proteins[protein])
-        output.write('\n')
+if __name__ == "__main__":
+    import os
+    import argparse
+    myParser = argparse.ArgumentParser(description='Extract CDS from genbank file')
+    myParser.add_argument('path_gbk', help='path to your genbank file')
+    args = myParser.parse_args()
+    input = args.path_gbk
+    output = f'{os.path.splitext(input)[0]}_proteins.fa'
+    seqIn = SeqIO.parse(input, 'genbank')
+
+    proteins = []
+    for seq in seqIn:
+        proteins.extend(getProteins(seq))
+    print(f"Total CDS {len(proteins)}")
+
+    SeqIO.write(proteins, output, 'fasta')
+    print(f'Done, check file\n{output}')
